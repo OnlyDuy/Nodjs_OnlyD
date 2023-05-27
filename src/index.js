5caabfc79cd2f8018533cb795b1b4c78b44c91af
OnlyDuy <vuquangduy002@gmail.com>
2023-05-27 17:52:36 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars').engine;
+const app = express();
+const port = 3000;
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/home', (req, res) => {
+    res.render('home');
+})
+
+app.get('/news', (req, res) => {
+    res.render('news');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+// Nodemon để lăng nghe sự thay đổi của những file trong source code để reaload lại mà không cần Ctrl C
+// Giúp cho chúng ta dễ dàng reload code hơn mà ko cần thoát server để reload lại
+
+// Morgan: thư viện quan sát được các log, request từ phía client lên Node server của mình
+
+//=======================================================================================================//
+// KIẾN THỨC CỐT LÕI:
+
+//10. Template engine (handlebars): Giúp bạn có thể viết ra các file chứa html ở những nới khác
+    // gọn gàn hơn đạt được hiệu quả khi code HTML thủ công
