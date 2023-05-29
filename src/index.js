c6db2db6678969ab797e490c1ad9a64dcda9377b
OnlyDuy <vuquangduy002@gmail.com>
2023-05-29 23:41:16 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middleware
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.json());
+
+
 // HTTP logger
 app.use(morgan('combined'));
 
@@ -19,15 +26,25 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/home', (req, res) => {
     res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
     res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+    //console.log(req.query.q);
+    res.render('search');
+});
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send('');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
 
 // Nodemon để lăng nghe sự thay đổi của những file trong source code để reaload lại mà không cần Ctrl C
 // Giúp cho chúng ta dễ dàng reload code hơn mà ko cần thoát server để reload lại
@@ -45,3 +62,15 @@ app.listen(port, () => {
     // - Cấu hình được CSS Preprocessors ( sd được những file như .scss)
 
 //12. Use Bootstrap
+
+//13. Basic routing: định nghĩa ra các tuyến đường giúp bạn truy cập vào website của chúng ta
+        //- Biến request: chưa thông tin của tuyển đường (PATH) mà nó gửi lên server
+        //- Biến response: chứa thông tin được trả về khi client yêu cầu. Giúp bạn tùy chọn, cấu hình, setup 
+                // làm sao để cho việc client trả về ntn, trả về cái gì
+            
+//14. Query parameters: khái niệm truyền dữ liệu qua chính url
+
+//16. Form default behavior: Hành vi mặc định của form: khi ta nhấn vào ô input có submit thì nó sẽ đính tất cả
+    // ô input có name trong form đó lên trên query parameter với phương thức là GET
+
+//17. POST method: sử dụng khi muốn gửi dữ liệu từ client lên server và không gử qua url (GET thì có gửi qua url)
