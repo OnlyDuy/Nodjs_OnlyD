c1ec3e575b82eedf4eee21478faebb9dfb3f3ad0
OnlyDuy <vuquangduy002@gmail.com>
2023-06-15 11:07:49 +0700

    add level to View and Operation

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -9,6 +9,7 @@ const Schema = mongoose.Schema;
 const Course = new Schema({
     name: { type: 'string', require: true},
     description: { type: 'string', maxLength: 600},
+    level: { type: 'string'},
     image: { type: 'string'},
     videoId: { type: 'string', require: true},
     slug: { type: String, slug: 'name'},
